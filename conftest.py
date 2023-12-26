44630e9eae269453afb08741b6db65f4eaa45612
Slava <smikhalitsynge@gmail.com>
2023-12-26 22:08:37 +0100

    working dockerfile

--- conftest.py
+++ conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
+@pytest.fixture
+def options():
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    yield options 
 
 @pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
+def driver(options):
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit() 
 
